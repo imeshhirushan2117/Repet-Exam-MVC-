0f0dc5cefb7d90fc223188fb8f5f76e49a24de48
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2022-09-20 10:15:00 +0530

    Student View Done

---
+++ src/Controller/StudentFormController.java
@@ -0,0 +1,31 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class StudentFormController {
+    public TextField txtSid;
+    public TextField txtName;
+    public TextField txtEmail;
+    public TextField txtContact;
+    public TextField txtNic;
+    public TextField txtAddress;
+    public TableView tblStudent;
+    public TableColumn colSid;
+    public TableColumn colName;
+    public TableColumn colNic;
+    public TableColumn colContact;
+    public TableColumn colEmail;
+    public TableColumn colAddress;
+
+    public void btnStudentSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStudentUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStudentDeleteOnAction(ActionEvent actionEvent) {
+    }
+}
